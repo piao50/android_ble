c3263f26cc9e8e1690bd6e7f6085ae963506afdf
piao50 <piao50@gmail.com>
2018-01-16 21:17:10 +0800

    gushi

--- app/src/main/java/com/example/gushi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gushi/myapplication/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.gushi.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        edit = (EditText)findViewById(R.id.editText);
+        findViewById(R.id.button_Test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                edit.setText("OK");
+                getResources().getString(R.string.msg);
+            }
+        });
     }
 }
